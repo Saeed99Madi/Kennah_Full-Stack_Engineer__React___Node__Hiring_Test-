430e18f4e6919f1b62b9335279a197683d52cf18
Saeed99Madi <saed.zooro@gmail.com>
2025-04-15 12:35:52 +0300

    connect to the database and create the schemas related to #3

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { DatabaseConfig } from './database/database.config';
 
 @Module({
-  imports: [],
+  imports: [DatabaseConfig, UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
