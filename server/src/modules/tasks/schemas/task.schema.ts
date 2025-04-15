829745a63898a594e3cc78d9bf2403131d24c374
Saeed99Madi <saed.zooro@gmail.com>
2025-04-15 13:23:15 +0300

    Build a CRUD system for tasks #9
    related to #9

--- server/src/modules/tasks/schemas/task.schema.ts
+++ server/src/modules/tasks/schemas/task.schema.ts
@@ -1,16 +1,28 @@
-import { Schema } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export const TaskSchema = new Schema(
-  {
-    title: { type: String, required: true },
-    description: { type: String },
-    status: {
-      type: String,
-      enum: ['pending', 'in-progress', 'completed'],
-      default: 'pending',
-    },
-    dueDate: { type: Date },
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  },
-  { timestamps: true },
-);
+export type TaskDocument = Task & Document;
+
+@Schema({ timestamps: true })
+export class Task {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({
+    type: String,
+    enum: ['pending', 'in-progress', 'completed'],
+    default: 'pending',
+  })
+  status: string;
+
+  @Prop()
+  dueDate: Date;
+
+  @Prop({ type: String, ref: 'User', required: true })
+  userId: string;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
